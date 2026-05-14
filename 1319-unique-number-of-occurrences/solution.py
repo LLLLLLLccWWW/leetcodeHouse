d38e10e754b5decb6e12d3398d2bd2b761b2ba28
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-05-14 05:40:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (99.08%)

---
+++ 1319-unique-number-of-occurrences/solution.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count = Counter(arr)
+        occurrences = list(count.values())
+        return len(occurrences) == len(set(occurrences))
