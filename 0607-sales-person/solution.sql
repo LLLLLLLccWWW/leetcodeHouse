4c22f18d91e2d910a37df3110aa542df3edf7a9f
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-05-26 03:16:50 +0000

    Sync LeetCode submission Runtime - 1383 ms (86.10%), Memory - 0.0B (100.00%)

---
+++ 0607-sales-person/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT name FROM SalesPerson s WHERE NOT EXISTS(
+    SELECT 1
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+    AND o.sales_id = s.sales_id
+)
