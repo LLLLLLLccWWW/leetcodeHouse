6fd342ab0f0d5a834311f503af4d25e24a9c23b4
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-05-28 01:36:51 +0000

    Sync LeetCode submission Runtime - 247 ms (60.00%), Memory - 13.3 MB (75.65%)

---
+++ 0661-image-smoother/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def imageSmoother(self, img):
+        """
+        :type img: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m,n = len(img), len(img[0])
+        res = [[0] * n for _  in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                total, count = 0,0
+                for di in [-1,0,1]:
+                    for dj in [-1,0,1]:
+                        ni,nj = i + di,j + dj
+                        if 0 <= ni < m and 0 <= nj < n:
+                            total += img[ni][nj]
+                            count += 1
+                res[i][j] = total // count
+
+        return res
