80093df9f2ef16c63e4422dafddbb665cb9621c2
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-05-28 06:29:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (9.90%)

---
+++ 1619-path-crossing/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPathCrossing(self, path):
+        """
+        :type path: str
+        :rtype: bool
+        """
+        visited = {(0,0)}
+        x,y = 0,0
+        direction = {'N':(0,1),'S':(0,-1),'E':(1,0),'W':(-1,0)}
+
+        for c in path:
+            dx,dy = direction[c]
+            x,y = x + dx, y + dy
+            if(x,y) in visited:
+                return True
+            visited.add((x,y))
+
+        return False
