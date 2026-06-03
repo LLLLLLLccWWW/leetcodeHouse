144bdd166bddc75f5ee5d8bf2910ce1109166d77
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-06-03 07:35:13 +0000

    Sync LeetCode submission Runtime - 755 ms (57.37%), Memory - 18.4 MB (59.39%)

---
+++ 0015-3sum/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue    # 跳過重複的第一個數
+
+            l,r = i + 1, len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total == 0:
+                    res.append([nums[i],nums[l],nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return res
