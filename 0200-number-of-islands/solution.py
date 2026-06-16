0884b5d0a6a606b03a4f91395f5d070ad2b0bd0f
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-06-16 09:05:18 +0000

    Sync LeetCode submission Runtime - 208 ms (93.32%), Memory - 27.7 MB (87.27%)

---
+++ 0200-number-of-islands/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+
+        m,n = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r,c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] != '1':
+                return
+            grid[r][c] = '0' # 標記已訪問
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(r,c)
+
+        return count
