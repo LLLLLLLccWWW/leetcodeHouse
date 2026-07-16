24ae0eb17d204901633ec1927de6776da2727b90
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-07-16 03:37:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (91.04%)

---
+++ 1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        t0,t1,t2 = 0,1,1
+
+        for _ in range(3,n+1):
+            next_t = t0 + t1 + t2
+
+            t0,t1,t2 = t1,t2,next_t
+
+        return t2
