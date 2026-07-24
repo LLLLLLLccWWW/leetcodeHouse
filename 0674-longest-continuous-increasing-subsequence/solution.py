3203720f69161e6fc464d82cd928cf435ae08c88
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-07-24 07:38:03 +0000

    Sync LeetCode submission Runtime - 3 ms (75.53%), Memory - 13.3 MB (65.68%)

---
+++ 0674-longest-continuous-increasing-subsequence/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        max_len = 1
+        curr_len = 1
+
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i - 1]:
+                curr_len += 1
+                max_len = max(max_len,curr_len)
+            else:
+                # 遞增中斷，重置當前長度
+                curr_len = 1
+
+        return max_len
