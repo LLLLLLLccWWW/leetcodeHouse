ae0517869346ec1f92cee6024cfe208d754d6f65
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-07-28 06:11:41 +0000

    Sync LeetCode submission Runtime - 8 ms (96.33%), Memory - 16.1 MB (37.78%)

---
+++ 1039-find-the-town-judge/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        scores = [0] * (n + 1)
+
+        for a,b in trust:
+            scores[a] -= 1
+            scores[b] += 1
+
+        for i in range(1,n + 1):
+            if scores[i] == n - 1:
+                return i
+
+        return -1
