ea8cbc0214d3a9f0d2e49038e5e44d63c0882cac
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-07-30 06:45:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.5 MB (37.37%)

---
+++ 0783-search-in-a-binary-search-tree/solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        curr = root
+        while curr and curr.val != val:
+            if val < curr.val:
+                curr = curr.left # 左小
+            else:
+                curr = curr.right # 右大
+
+        return curr
