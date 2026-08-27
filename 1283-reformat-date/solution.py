43444728bddd52ac9b6f9e4e9c37f438dece3b05
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-08-27 06:12:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (61.42%)

---
+++ 1283-reformat-date/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reformatDate(self, date):
+        """
+        :type date: str
+        :rtype: str
+        """
+        month_map = {
+            "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04",
+            "May": "05", "Jun": "06", "Jul": "07", "Aug": "08",
+            "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"
+        }
+
+        day,month,year = date.split()
+
+        day_num = day[:-2]  # 去掉最後兩個字元（st/nd/rd/th）
+        day_num = day_num.zfill(2)  # 補齊成 2 位數
+
+        month_num = month_map[month]
+
+        return "%s-%s-%s" % (year,month_num,day_num)
