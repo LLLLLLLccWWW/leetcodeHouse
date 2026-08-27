16004e094e11af6359e5ca391af3e9499f508bfd
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-08-27 06:04:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (84.28%)

---
+++ 1626-can-make-arithmetic-progression-from-sequence/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort()
+        common_diff = arr[1] - arr[0]
+
+        for i in range(2,len(arr)):
+            if arr[i] - arr[i - 1] != common_diff:
+                return False
+            
+        return True
