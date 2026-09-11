936149a728d3ab9d0c8e57ce22a8817f01c77844
劉哲瑋 <123094115+LLLLLLLccWWW@users.noreply.github.com>
2026-09-11 07:05:03 +0000

    Sync LeetCode submission Runtime - 205 ms (23.68%), Memory - 14.4 MB (70.53%)

---
+++ 1786-count-the-number-of-consistent-strings/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        allowed_set = set(allowed)
+
+        # 統計所有字元都在 allowed_set 中的單字數量
+        return sum(all(char in allowed_set for char in word) for word in words)
